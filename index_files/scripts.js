60667c3039821e01a47097f616bf39de85b1f456
Artemissssss <64923345+Artemissssss@users.noreply.github.com>
2022-01-27 15:17:49 +0200

    Update scripts.js

--- index_files/scripts.js
+++ index_files/scripts.js
@@ -4,7 +4,7 @@ document.getElementById("btn").addEventListener("click",()=>{
 	let second = `Второе худи:${document.getElementById("field2").value}`
 	let name = `Имя:${document.getElementById("name").value}`
 	let tel = `Телефон:${document.getElementById("tel").value}`
-	document.getElementById("btn").href = `mailto:modernbiz321@gmail.com?subject=Заказ худи&body=${first}\n${second}\n${name}\n${tel}`
+	document.getElementById("btn").href = `mailto:modernbiz321@gmail.com?subject=Заказ худи&body=${first}%0D%0A${second}%0D%0A${name}%0D%0A${tel}`
 	document.getElementById("btn").click()
 	if(r==1){
 	document.getElementById("btn").href=""
